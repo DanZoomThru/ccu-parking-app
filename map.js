36e5604d6452fd4d1219b30a0ff38471477395b8
CJGARLING <cjgarling@g.coastal.edu>
2016-11-08 21:56:16 -0500

    updated map.js

--- map.js
+++ map.js
@@ -13,10 +13,10 @@
         // As we expand to more lots, more markers will open up
         var lotBB = {lat: 33.799678,lng:  -79.012966};
         var lotDD = {lat: 33.799320,lng:  -79.013819};
-        // var lotEE1 = {lat: ,lng: };
-        // var lotEE2 = {lat: ,lng: };
-        // var lotAA1 = {lat: ,lng: };
-        // var lotAA2 = {lat: ,lng: };
+        var lotEE1 = {lat:33.800034,lng:  -79.012431};
+        var lotEE2 = {lat: 33.797983,lng:  -79.010578};
+        var lotAA1 = {lat: 33.798344,lng:  -79.016367};
+        var lotAA2 = {lat: 33.799501,lng:  -79.016179};
         // var lotA = {lat: ,lng: };
         // var lotA = {lat: ,lng: };
         // var lotA = {lat: ,lng: };
@@ -32,21 +32,27 @@
         var lotGG1 = {lat: 33.796556, lng: -79.006727};
         var lotGG2 = {lat: 33.796096, lng: -79.007530};
         var lotGG3 = {lat: 33.796808, lng: -79.008308};
-
+        //
+        //Set scrolling bounds for viewing on the map
+        //
+        // var strictBounds = new google.maps.LatLngBounds(
+        //   new google.maps.LatLng(33.781755, -79.026981),
+        //   new google.maps.LatLng(33.801691, -78.988061)
+        //   );
         var map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 33.7950, lng: -79.0117},
-          //prevents zooming with +/- buttons
-          zoomControl: false,
-          //prevents zooming by double-left-clicking
-          disableDoubleClickZoom: true,
-          //prevents scrolling with mouse
-          scrollwheel: false,
-          //disable street view 
+          // //prevents zooming with +/- buttons
+          // zoomControl: false,
+          // //prevents zooming by double-left-clicking
+          // disableDoubleClickZoom: true,
+          // //prevents scrolling with mouse
+          // scrollwheel: false,
+          // //disable street view 
           streetViewControl: false,
+          minZoom:16,
+          maxZoom:18,
 
           center: {lat: -34.397, lng: 150.644},
-          zoomControl: false,
-          scrollwheel: false,
           mapTypeControl: true,
           mapTypeConrolOptions:{
           style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
@@ -55,12 +61,87 @@
 
           //zoom with best view of campus parkinglots
           zoom: 16
-        })
-        var marker = new google.maps.Marker({
-          position: lotGG,
+        });
+        var markerGG1 = new google.maps.Marker({
+          position: lotGG1,
           map: map,
-          title: 'Hello World!'
-        })
+          title: 'Lot GG'
+        });
+        var markerGG2 = new google.maps.Marker({
+          position: lotGG2,
+          map: map,
+          title: 'Lot GG'
+        });
+        var markerGG3 = new google.maps.Marker({
+          position: lotGG3,
+          map: map,
+          title: 'Lot GG'
+        });
+        var markerKK1 = new google.maps.Marker({
+          position: lotKK1,
+          map: map,
+          title: 'Lot KK'
+        });
+        var markerKK2 = new google.maps.Marker({
+          position: lotKK2,
+          map: map,
+          title: 'Lot KK'
+        });
+        var markerG = new google.maps.Marker({
+          position: lotG,
+          map: map,
+          title: 'Lot G'
+        });
+        var markerQQ1 = new google.maps.Marker({
+          position: lotQQ1,
+          map: map,
+          title: 'Lot QQ'
+        });
+        var markerQQ2 = new google.maps.Marker({
+          position: lotQQ2,
+          map: map,
+          title: 'Lot QQ'
+        });
+        var markerDD = new google.maps.Marker({
+          position: lotDD,
+          map: map,
+          title: 'Lot DD'
+        });
+        var markerBB = new google.maps.Marker({
+          position: lotBB,
+          map: map,
+          title: 'Lot BB'
+        });
+        var markerHH1 = new google.maps.Marker({
+          position: lotHH1,
+          map: map,
+          title: 'Lot HH'
+        });
+        var markerHH2 = new google.maps.Marker({
+          position: lotHH2,
+          map: map,
+          title: 'Lot HH'
+        });
+        var markerAA1 = new google.maps.Marker({
+          position: lotAA1,
+          map: map,
+          title: 'Lot AA'
+        });
+        var markerAA2 = new google.maps.Marker({
+          position: lotAA2,
+          map: map,
+          title: 'Lot AA'
+        });
+        var markerEE1 = new google.maps.Marker({
+          position: lotEE1,
+          map: map,
+          title: 'Lot EE'
+        });
+        var markerEE2 = new google.maps.Marker({
+          position: lotEE2,
+          map: map,
+          title: 'Lot EE'
+        });
         var infoWindow = new google.maps.InfoWindow({map: map});
         // Try HTML5 geolocation.
         // Geolocation is what finds your current location. Must be approved in browser.
